3f09eb34a4f6795b66a655c2b1abaabbe9907773
vkuruvan <vkuruvan@VKURUVIN-WS02.partnet.cisco.com>
2018-12-13 12:56:19 +0530

    comminting the changes

--- DevOpsModule_POC/src/test/java/DevOpsModule_POC/DevOpsModule_POC/Mobile_Automation_POC.java
+++ DevOpsModule_POC/src/test/java/DevOpsModule_POC/DevOpsModule_POC/Mobile_Automation_POC.java
@@ -2,6 +2,7 @@ package DevOpsModule_POC.DevOpsModule_POC;
 
 import java.net.URL;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
@@ -10,8 +11,8 @@ import io.appium.java_client.android.AndroidDriver;
 public class Mobile_Automation_POC {
 	AndroidDriver driver;
 
-	@Test(priority = 1)
-	public boolean Mobile_Automation()
+	@Test
+	public void Mobile_Automation()
 	{
 		try
 		{
@@ -29,14 +30,23 @@ public class Mobile_Automation_POC {
 	        
 	        driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"),capabilities);
 	        
+	        System.out.println("Starting inputting the data in to mobile app");
+	        
+	       // String MobileTitel = driver.getTitle();
+	      //  System.out.println("The Title is"+MobileTitel);
+	        driver.findElement(By.id("org.wikipedia:id/search_container")).clear();
+	        Thread.sleep(5000);
+	        driver.findElement(By.id("org.wikipedia:id/search_container")).sendKeys("Wipro Technologies");
+	        System.out.println("Input data  entred sucessfully");
+	        
+	        Thread.sleep(10000);
 	        driver.quit();
 	        
 	        }
 		catch(Exception e)
 		{
+			e.printStackTrace();
 			System.out.println("Exception");
-			return false;
 		}
-		return true;
 	}
 }
--- DevOpsModule_POC/src/test/java/DevOpsModule_POC/DevOpsModule_POC/POC_DEMO_Tests.java
+++ DevOpsModule_POC/src/test/java/DevOpsModule_POC/DevOpsModule_POC/POC_DEMO_Tests.java
@@ -1,41 +1,93 @@
 package DevOpsModule_POC.DevOpsModule_POC;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import java.sql.*;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import com.google.common.collect.Table;
+import com.google.common.collect.Tables;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 public class POC_DEMO_Tests {
 	
 	WebDriver driver;
+	public ExtentReports extent;
+	public ExtentTest test;
 	boolean rc;
 	
 
-	
-	
+
+public boolean  db_connect()
+{
+	try
+	{
+		//System.out.println("test");
+		//Class.forName("oracle.jdbc.driver.OracleDriver");
+		//System.out.println("after connecting");
+		//System.out.println("Driver connected sucessfully");
+		//DriverManager.setLoginTimeout(10);
+		
+		//Connection con = DriverManager.getConnection("jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS_LIST=(LOAD_BALANCE=ON)(FAILOVER=ON)(ADDRESS=(PROTOCOL=TCP)(HOST=lnxdb-stg-vm-347-vip.cisco.com)(PORT=1525))(ADDRESS=(PROTOCOL=TCP)(HOST=lnxdb-stg-vm-346-vip.cisco.com)(PORT=1525)))(CONNECT_DATA=(SERVICE_NAME=ALMSTG.cisco.com)(SERVER=DEDICATED)))","QACAUTOUSR","1b37W96e");
+		//System.out.println("DB Connected sucessfully");
+		
+		//Statement stmt = con.createStatement();	
+		
+		//System.out.println("Started feteching the quries");
+		
+		//Table Resultdata = (Table) stmt.executeQuery("select * from  cits_asr12_testdata  where SCENARIO_NAME='CRM_Acc_Opp_Creation'");
+		
+		Class.forName("oracle.jdbc.OracleDriver");
+		//Class.forName("oracle.jdbc.driver.OracleDriver");  
+		//DriverManager.setLoginTimeout(10);
+		Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS_LIST=(LOAD_BALANCE=ON)(FAILOVER=ON)(ADDRESS=(PROTOCOL=TCP)(HOST=lnxdb-stg-vm-347-vip.cisco.com)(PORT=1525))(ADDRESS=(PROTOCOL=TCP)(HOST=lnxdb-stg-vm-346-vip.cisco.com)(PORT=1525)))(CONNECT_DATA=(SERVICE_NAME=ALMSTG.cisco.com)(SERVER=DEDICATED)))","QACAUTOUSR","1b37W96e");
+		//Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS_LIST=(LOAD_BALANCE=ON)(FAILOVER=ON)(ADDRESS=(PROTOCOL=TCP)(HOST=64.101.41.144)(PORT=1541))(ADDRESS=(PROTOCOL=TCP)(HOST=64.101.41.145)(PORT=1541)))(CONNECT_DATA=(SERVICE_NAME=OPSRSTG.cisco.com)(SERVER=DEDICATED)))","ATOMOCUSR","atOM18db");
+		if(conn!=null){
+		System.out.print("Connection Successful");
+		}else{
+		System.out.print("Connection Unsuccessful");
+		}
+		Statement stmt= conn.createStatement();
+		String Sname ="CRM_Acc_Opp_Creation";
+		String strQuery="select * from  cits_asr12_testdata  where SCENARIO_NAME='"+Sname+"'";
+		ResultSet sr = stmt.executeQuery(strQuery);
+		while(sr.next())
+		{
+		String test = sr.getString("TEST_CASE_NAME");
+		System.out.println(test);
+		}
+		return true;
+		}
+		
+	catch(Exception e)
+	{
+		
+		System.out.println("The exception is"+e);
+		return false;
+	}
+}
 public boolean create_AccDetails(){
 		try
 		{
-			Thread.sleep(10000);
+			Thread.sleep(20000);
 			WebElement Pcontact = driver.findElement(By.xpath("//input[@id ='_FOpt1:_FOr1:0:_FOSritemNode_service_accounts:0:_FOTsr1:0:pt1:r22:0:pt1:preferredContactNameId::content']"));
 			Pcontact.sendKeys("Shruthi B K");
 			
 			
-			Thread.sleep(10000);
+			
+			Thread.sleep(20000);
 			Select type = new Select(driver.findElement(By.xpath("//select[@id ='_FOpt1:_FOr1:0:_FOSritemNode_service_accounts:0:_FOTsr1:0:pt1:r22:0:pt1:sor1::content']")));
 			type.selectByVisibleText("Customer");
 			
 			//Selecting the address
-			Thread.sleep(10000);
+			Thread.sleep(20000);
 			Select Country = new Select(driver.findElement(By.xpath("//select[@id ='_FOpt1:_FOr1:0:_FOSritemNode_service_accounts:0:_FOTsr1:0:pt1:r22:0:pt1:r4:0:r1:0:s2:LocationCountry::content']")));
 			Country.selectByValue("39");
 			
-			Thread.sleep(10000);
+			Thread.sleep(20000);
 			WebElement Address = driver.findElement(By.xpath("//input[@id='_FOpt1:_FOr1:0:_FOSritemNode_service_accounts:0:_FOTsr1:0:pt1:r22:0:pt1:r4:0:r1:0:s2:i1:5:inputText2::content']"));
 			Address.sendKeys("Postmaster, Post Office CARMELRAM (SUB OFFICE), BANGALORE, KARNATAKA (KA), India (IN), Pin Code:- 560035");
 			
@@ -47,15 +99,17 @@ public boolean create_AccDetails(){
 			
 			WebElement postlcode = driver.findElement(By.xpath("//input[@id = '_FOpt1:_FOr1:0:_FOSritemNode_service_accounts:0:_FOTsr1:0:pt1:r22:0:pt1:r4:0:r1:0:s2:i1:10:inputText7::content']"));
 			postlcode.sendKeys("560035");
-			Thread.sleep(10000);
+			Thread.sleep(20000);
+			test.log(LogStatus.PASS, "Enter the Valid Address", "Valid address Entered Sucessfully");
 			
 			
 			 
-			Thread.sleep(10000);
+			Thread.sleep(20000);
 			//Clicking on the Save and COntinue
 			WebElement button = driver.findElement(By.xpath("//button[@id='_FOpt1:_FOr1:0:_FOSritemNode_service_accounts:0:_FOTsr1:0:pt1:r22:0:pt1:AP1:cb2']"));
 			button.click();
-			Thread.sleep(15000);
+			Thread.sleep(20000);
+			test.log(LogStatus.PASS, "Click on the Save and Continue Button", "User Entered the Save and Continue button Sucessfully");
 			
 			//Getting the Opportunity name and validating the condition
 			WebElement AccountName = driver.findElement(By.xpath("//input[@id = '_FOpt1:_FOr1:0:_FOSritemNode_service_accounts:0:_FOTsr1:0:pt1:r22:1:pt1:r1:0:r1:0:it4::content']"));
@@ -70,6 +124,7 @@ public boolean create_AccDetails(){
 			WebElement opportunity = driver.findElement(By.xpath("//div[@id = '_FOpt1:_FOr1:0:_FOSritemNode_service_accounts:0:_FOTsr1:0:pt1:r22:1:pt1:Opportunities::text']"));
 			opportunity.click();
 			Thread.sleep(15000);
+			test.log(LogStatus.PASS, "Click on the Opportunity Button", "Opprtunity button Entered Sucessfully");
 			
 			WebElement createopp = driver.findElement(By.xpath("//button[@id='_FOpt1:_FOr1:0:_FOSritemNode_service_accounts:0:_FOTsr1:0:pt1:r22:1:pt1:r4:1:stsSrch:create-opportunity-commandButton']"));
 			createopp.click();
@@ -77,23 +132,24 @@ public boolean create_AccDetails(){
 			
 		System.out.println("Entered the Opportunity Page");
 			
-		        Thread.sleep(10000);
+		        Thread.sleep(20000);
 				WebElement OppName = driver.findElement(By.xpath("//input[@id ='_FOpt1:_FOr1:0:_FOSritemNode_service_accounts:0:_FOTsr1:0:pt1:r22:1:pt1:r4:1:r3:0:pt1:it1::content']"));
 			    OppName.sendKeys("POC_Demo_Opportunity");
+			    test.log(LogStatus.PASS, "Verify the Opportunity Name", "Opprtunity Name entered Sucessfully");
 			    
 			    
-				Thread.sleep(10000);
+				Thread.sleep(20000);
 			    WebElement PrimaryContact = driver.findElement(By.xpath("//input[@id ='_FOpt1:_FOr1:0:_FOSritemNode_service_accounts:0:_FOTsr1:0:pt1:r22:1:pt1:r4:1:r3:0:pt1:primaryContact::content']"));
 			    PrimaryContact.sendKeys("Shruthi B K");
 			    
-			    Thread.sleep(10000);
+			    Thread.sleep(20000);
 			    WebElement Savebutt = driver.findElement(By.xpath("//button[@id = '_FOpt1:_FOr1:0:_FOSritemNode_service_accounts:0:_FOTsr1:0:pt1:r22:1:pt1:r4:1:r3:0:pt1:AP1:cb3']"));
 			    Savebutt.click();
-			    Thread.sleep(10000);
+			    Thread.sleep(20000);
 			    
 			    WebElement SummrySavebutt = driver.findElement(By.xpath("//button[@id='_FOpt1:_FOr1:0:_FOSritemNode_service_accounts:0:_FOTsr1:0:pt1:r22:1:pt1:r4:1:r3:1:pt1:object-subtitle:cb11']"));
 			    SummrySavebutt.click();
-			    Thread.sleep(10000);
+			    Thread.sleep(20000);
 			    
 			    //Validating the Opportunity name
 			    
@@ -102,12 +158,13 @@ public boolean create_AccDetails(){
 			    System.out.println("The Oppaname is"+Oppname1);
 			    
 			    //Opoortunity Summary Page
-			    Thread.sleep(10000);
+			    Thread.sleep(20000);
 			    WebElement OppSave =  driver.findElement(By.xpath("//button[@id='_FOpt1:_FOr1:0:_FOSritemNode_service_accounts:0:_FOTsr1:0:pt1:r22:1:pt1:r4:1:r3:1:pt1:object-subtitle:cb1']"));
 			    OppSave.click();
+			    test.log(LogStatus.PASS, "Verify the Opportunity Summay Page Deatails", "Oportunity Summay Page details verified sucessfully");
 			    
 			    //Opportunities
-			    Thread.sleep(10000);
+			    Thread.sleep(20000);
 			    WebElement Oppsave1 = driver.findElement(By.xpath("//button[@id='_FOpt1:_FOr1:0:_FOSritemNode_service_accounts:0:_FOTsr1:0:pt1:r22:1:pt1:object-subtitle:cb8']"));
 			    Oppsave1.click();
 			    
@@ -124,11 +181,30 @@ public boolean create_AccDetails(){
 //public void beforeTest() {	
     //}		
 
-@Test(priority = 2)
+@Test
 	public void Login () {
 		System.out.println("Test1");
 		try 
 		{
+			
+			//DB Connection
+			
+			rc = db_connect();
+			Thread.sleep(50000);
+			if(rc==true)
+			{
+				System.out.println("Sucessfully able to get the test data from DB");
+			//String Username = (String) Tableresultdata.get(1, "R12_USER_NAME");
+			//System.out.println("User name is"+Username);
+				
+   //Report Generation
+				
+				extent = new ExtentReports("C:\\Selinium_javadrivers\\Reports\\TestResult.html", true);
+				//C:\Selinium_javadrivers\Reports
+				
+				test = extent.startTest("Launching the browser");
+				test.log(LogStatus.INFO, "Browser intiated sucessfully");
+			
 			//driver = new ChromeDriver();  
 			System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir")+"\\geckodriver.exe");
 			//WebDriver driver = new ChromeDriver();
@@ -156,12 +232,15 @@ public boolean create_AccDetails(){
 			username.sendKeys(username1);
 			Thread.sleep(8000);
 			System.out.println("User Name entered sucessfully");
+			test.log(LogStatus.PASS, "Enter the User Name", "User Name Entered Sucessfully"+username1);
+			
 			
 			String Password1 = "Ashwitha@123";
 	        WebElement password = driver.findElement(By.id("password"));
 			password.sendKeys(Password1);
 			Thread.sleep(8000);
 			System.out.println("Password entered Sucessfully");
+			test.log(LogStatus.PASS, "Enter the User Name", "User Name Entered Sucessfully"+Password1);
 			
 			
 			WebElement LoginButton = driver.findElement(By.xpath("//button[contains(text(),'Sign In')]"));
@@ -169,6 +248,7 @@ public boolean create_AccDetails(){
 			Thread.sleep(8000);
 			
 			System.out.println("Logged in sucessfully");
+			test.log(LogStatus.PASS, "Verify user logged in Sucessfully", "User Sucessfully logged in to the Engagement Cloud");
 			
 			/*WebElement Salesbutton = driver.findElement(By.id("groupNode_sales"));
 			System.out.println("Before clicking on the button");
@@ -179,7 +259,7 @@ public boolean create_AccDetails(){
 			Thread.sleep(8000);
 			WebElement Navigator = driver.findElement(By.xpath("//a[@id = 'pt1:_UISmmLink']"));
 			Navigator.click();
-			Thread.sleep(15000);
+			Thread.sleep(30000);
 			
 			//Creating the Account
 			 WebElement Accbutton = driver.findElement(By.xpath("//a[@id = 'pt1:nv_itemNode_service_accounts']"));
@@ -201,8 +281,10 @@ public boolean create_AccDetails(){
 				 
 				 WebElement Accname = driver.findElement(By.xpath("//input[@id='_FOpt1:_FOr1:0:_FOSritemNode_service_accounts:0:_FOTsr1:0:pt1:r22:0:pt1:it1::content']"));
 				 Accname.sendKeys("POC_DEMO_SalesCloud");
+				 String Acname = "POC_DEMO_SalesCloud";
 				 System.out.println("Sucessfully Entered the Account Name");
-				 Thread.sleep(15000);
+				 Thread.sleep(20000);
+				 test.log(LogStatus.PASS, "Enter the Account Name", "Account Name Entered Sucessfully"+Acname);
 				/* WebElement Savebutton = driver.findElement(By.xpath("//button[@id='_FOpt1:_FOr1:0:_FONSr2:0:_FOTsr1:0:pt1:r22:0:pt1:AP1:cb3']"));
 				 Savebutton.click();
 				 System.out.println("Sucessfully Created the Account Name");*/
@@ -223,9 +305,15 @@ public boolean create_AccDetails(){
 			 arrowmark.click();
 			 Thread.sleep(10000);
 			 WebElement Signout = driver.findElement(By.xpath("//a[@id='_FOpt1:_UISlg1']"));
+			 test.log(LogStatus.PASS, "Click on the Signput Button", "User Sucessfully Signed out from the Application");
 			 Signout.click();
 			 System.out.println("Sucessfully Signed out from the applications");
+			 
+			 extent.endTest(test);
+			 extent.flush();
+			 
 			 driver.close();
+			}
 			
 		} catch (Exception e) {
 			e.printStackTrace();
